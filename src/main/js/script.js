b2e31b65fc4bb4e53f09c985dd9bf7daecedf696
IrsanJam <Irsandjamaludin@gmail.com>
2023-02-03 14:32:35 +0700

    menambah sedikit dark mode

--- src/main/js/script.js
+++ src/main/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const head = document.querySelector("header");
   const patokan = head.offsetTop;
+  const totop = document.querySelector("#totop");
 
   if (patokan < window.pageYOffset) {
     head.classList.add("navbar-fixed");
+    totop.classList.remove("hidden");
+    totop.classList.add("flex");
   } else {
     head.classList.remove("navbar-fixed");
+    totop.classList.remove("flex");
+    totop.classList.add("hidden");
   }
 };
 //ini bagian hamburger
@@ -17,5 +22,23 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navbarMenu.classList.toggle("hidden");
 });
-hamburger - active;
-hidden;
+
+// click diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target !== hamburger) {
+    hamburger.classList.remove("hamburger-active");
+    navbarMenu.classList.add("hidden");
+  }
+});
+
+// dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
+});
