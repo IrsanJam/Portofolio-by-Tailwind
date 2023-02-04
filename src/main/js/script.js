4173c88ee55603a9607aa03802ee15b5c6f7a777
IrsanJam <Irsandjamaludin@gmail.com>
2023-02-04 16:42:04 +0700

    menambah dark mode

--- src/main/js/script.js
+++ src/main/js/script.js
@@ -38,7 +38,17 @@ const html = document.querySelector("html");
 darkToggle.addEventListener("click", function () {
   if (darkToggle.checked) {
     html.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
     html.classList.remove("dark");
+    localStorage.theme = "light";
   }
 });
+
+// pintasan toggle sesuai mode
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  document.documentElement.classList.add("dark");
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
